cd6e6831d0189d4e871be327dc0e6f0b97d530a3
pbegle <peter.begle@bytecode.io>
2021-07-14 17:00:30 -0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,26 @@
+from sp_api.api import Inventories, Sales
+
+
+
+def sales_handler(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    return f'Hello World!'
+
+
+def inventories_handler(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    return f'Hello World!'
