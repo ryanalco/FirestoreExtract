133bc345e2189b0e334a5371ded3a81e967c1d16
0xpetersatoshi <0xpetersatoshi@protonmail.com>
2021-10-07 15:34:38 -0700

    feat(scripts): change cron job to run once a day

--- scripts/create-scheduler.sh
+++ scripts/create-scheduler.sh
@@ -4,7 +4,7 @@ SERVICE_URL=$1
 SERVICE_ACCOUNT_EMAIL=$2
 
 # usage: bash create-scheduler.sh SERVICE_URL SERVICE_ACCOUNT_EMAIL
-gcloud scheduler jobs create http invoke-firebase-extractor --schedule "0 * * * *" \
+gcloud scheduler jobs create http invoke-firebase-extractor --schedule "0 4 * * *" \
    --http-method=GET \
    --uri=${SERVICE_URL} \
    --oidc-service-account-email=${SERVICE_ACCOUNT_EMAIL} \
